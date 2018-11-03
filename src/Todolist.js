5fb4802eac196357fbd3921bb72f6fca540c5bae
Jasmin Bissig <jasmin.bissig@gmail.com>
2018-11-03 16:45:12 +0100

    Render todo items in a list

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import TodoItems from "./TodoItems";
 
 class TodoList extends Component {
 
@@ -44,8 +45,9 @@ class TodoList extends Component {
                         <button type="submit">add</button>
                     </form>
                 </div>
+                 <TodoItems entries={this.state.items}/>
             </div>
-        )
+        );
     }
 }
 
